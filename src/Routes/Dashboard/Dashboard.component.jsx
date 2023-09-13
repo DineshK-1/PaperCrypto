3af54585d013d3640480ef805d286fbfc2b3c0d4
Dinesh <dineshkumar.software@gmail.com>
2023-09-13 19:52:40 +0530

    Added vercel config

--- src/Routes/Dashboard/Dashboard.component.jsx
+++ src/Routes/Dashboard/Dashboard.component.jsx
@@ -15,20 +15,23 @@ const DashboardRoute = () => {
                 <AddMoney setModal={setModal} />
             }
 
-            <div className="dashboard-route flex flex-col font-medium text-lg cryptos-page">
+            <div className="dashboard-route flex flex-col font-medium gap-4 cryptos-page text-base">
                 <div className="flex gap-2 mt-10">
-                    <div className="flex p-6 gap-3 items-center card">
+                    <div className="flex p-6 gap-3 items-center card flex-1 justify-between">
+                        <div className="flex items-center gap-3">
                         <span className="material-symbols-outlined bg-indigo-800 rounded-lg p-2 text-slate-300">account_balance</span>
                         <div className="flex flex-col">
                             <span className="text-sm">Current Balance:</span>
                             <span className="blue font-semibold">${db_user.Current_Balance}</span>
                         </div>
+                        </div>
+                        
                         <span
                             className="material-symbols-outlined bg-green-600 rounded-lg p-1 text-white cursor-pointer"
                             onClick={() => { setModal(true) }}
                         >add</span>
                     </div>
-                    <div className="flex p-6 gap-3 items-center card">
+                    <div className="flex p-6 gap-3 items-center card flex-1">
                         <span className="material-symbols-outlined bg-indigo-800 rounded-lg p-2 text-slate-300">currency_bitcoin</span>
                         <div className="flex flex-col">
                             <span className="text-sm">Portfolio Value:</span>
@@ -37,18 +40,27 @@ const DashboardRoute = () => {
                     </div>
                 </div>
 
-                <div className="flex flex-col gap-4 p-4">
+                <div className="flex flex-col gap-4 p-4 card">
                     <span className="text-xl">Current Holdings</span>
                     <div className="flex text-gray-500">
                         <span>Get Started with Crypto today by clicking here!</span>
                     </div>
                 </div>
-                <div className="flex flex-col gap-4 p-4">
-                    <span className="text-xl">Transaction History</span>
-                    <div className="flex text-gray-500">
-                        <span>No Transactions yet</span>
+                <div className="flex gap-5">
+                    <div className="flex flex-col gap-4 p-4 px-6 card">
+                        <span className="text-xl">Crypto Transaction History</span>
+                        <div className="flex text-gray-500">
+                            <span>No transactions yet!</span>
+                        </div>
+                    </div>
+                    <div className="flex flex-col gap-4 p-4 px-6 card">
+                        <span className="text-xl">Fiat Transaction History</span>
+                        <div className="flex text-gray-500">
+                            <span>No deposits/withdrawals yet.</span>
+                        </div>
                     </div>
                 </div>
+
             </div>
         </Fragment>
     )
