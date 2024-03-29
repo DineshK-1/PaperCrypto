a3b84f1edec6e76761d398eb631530be4f06b5c4
Dinesh <dineshkumar.software@gmail.com>
2024-03-29 10:33:31 +0530

    Change default color mode

--- src/Components/NavBar/NavBar.component.jsx
+++ src/Components/NavBar/NavBar.component.jsx
@@ -1,12 +1,14 @@
 import { Fragment, useContext, useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { UserContext } from "../../Contexts/user.context";
+import { FaMoon, FaSun } from "react-icons/fa";
+import { IoSunnySharp } from "react-icons/io5";
 
 const NavBar = () => {
     const { user } = useContext(UserContext);
     const location = useLocation();
 
-    const [darkMode, setDarkMode] = useState(false);
+    const [darkMode, setDarkMode] = useState(true);
 
     useEffect(() => {
         if (darkMode) {
@@ -19,7 +21,9 @@ const NavBar = () => {
     return (
         <Fragment>
             <div className="navbar flex gap-3 p-4 items-center w-full justify-end">
-                <button onClick={() => { setDarkMode((e) => !e) }}>Toggle Modes</button>
+                <button onClick={() => { setDarkMode((e) => !e) }}>{
+                    darkMode ? <FaMoon /> : <IoSunnySharp />}
+                </button>
                 <Link to="/"><div className={"nav-item" + (location.pathname === "/" ? " active" : "")}>Home</div></Link>
                 {user &&
                     <Link to="/Dashboard"><div className={"nav-item" + (location.pathname === "/Dashboard" ? " active" : "")}>Dashboard</div></Link>
